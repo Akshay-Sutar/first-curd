e5db9136bf029764649c6f4a784c0a9ef832a5b5
Akshay Sutar <akshay.sutar4@gmail.com>
2021-09-21 18:12:39 +0530

    added test cases

--- test/task.test.js
+++ test/task.test.js
@@ -18,6 +18,7 @@ describe("Todo API", () => {
           done();
         });
     });
+
     it("should not get TODOs ", (done) => {
       chai
         .request(server)
@@ -31,15 +32,114 @@ describe("Todo API", () => {
 
   describe("GET /api/todo/:id", () => {
     it("should get specific todo", (done) => {
-        const id="6149a0f9c48fbc27bc12dba8";
+      const id = "6149c3b676ed4b61807391bb";
       chai
         .request(server)
-        .get("/api/todo/"+id)
+        .get("/api/todo/" + id)
         .end((err, response) => {
-            response.should.have.status(200);
-            response.body.should.be.a('object');
-            response.body.should.have.nested.property('docs[0]._id');
-            done();
+          response.should.have.status(200);
+          response.body.should.be.a("object");
+          response.body.should.have.nested.property("docs[0]._id");
+          done();
+        });
+    });
+
+    it("should return 404", (done) => {
+      const id = 1;
+      chai
+        .request(server)
+        .get("/api/todo/" + id)
+        .end((err, response) => {
+          response.should.have.status(400);
+          response.body.should.be.a("object");
+          done();
+        });
+    });
+  });
+
+  describe("POST /api/todo", () => {
+    it("should create a new todo", (done) => {
+      const todoItem = {
+        title: "Test Case 2",
+        description: "Test Case 2 is a POST API test case",
+      };
+      chai
+        .request(server)
+        .post("/api/todo")
+        .send(todoItem)
+        .end((err, response) => {
+          response.should.have.status(201);
+          response.body.should.be.a("object");
+          response.body.should.have.property("_id");
+          response.body.should.have.property("title").eq(todoItem.title);
+          done();
+        });
+    });
+
+    it("should not create a new todo", (done) => {
+      const todoItem = {
+        description: "Test Case 2 is a POST API test case",
+      };
+      chai
+        .request(server)
+        .put("/api/todo")
+        .send(todoItem)
+        .end((err, response) => {
+          response.should.have.status(404);
+          response.body.should.be.a("object");
+          response.body.should.have
+            .property("message")
+            .eq("No title specified!");
+          done();
+        });
+    });
+  });
+
+  describe("PUT /api/todo/:id", () => {
+    it("should update an existing todo", (done) => {
+      const todoItem = {
+        title: "Complete Mocha",
+        description: "Complete Mocha framework with Chai-Sinon",
+        completed: true,
+      };
+      const id = "6149c3b676ed4b61807391bb";
+      chai
+        .request(server)
+        .put("/api/todo/" + id)
+        .send(todoItem)
+        .end((err, response) => {
+          response.should.have.status(200);
+          response.body.should.be.a("object");
+          response.body.should.have.property("nModified").eq(1);
+          done();
+        });
+    });
+  });
+
+  describe("Delete /api/todo/:id", () => {
+    it("should delete an existing todo", (done) => {
+      const id = "6149c3b676ed4b61807391bb";
+      chai
+        .request(server)
+        .delete("/api/todo/" + id)
+        .end((err, response) => {
+          response.should.have.status(200);
+          response.body.should.be.a("object");
+          response.body.should.have.property("deletedCount").eq(1);
+          done();
+        });
+    });
+
+    it("should delete give 404 error", (done) => {
+      const id = 1;
+      chai
+        .request(server)
+        .delete("/api/todo/" + id)
+        .end((err, response) => {
+          response.should.have.status(400);
+          response.body.should.be.a("object");
+          response.body.should.have.property("message").eq("Invalid Id!");
+          done();
         });
     });
   });
